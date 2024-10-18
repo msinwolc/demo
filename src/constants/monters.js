16ad1c3975c2801c5a0e925da816091e7d5e08ca
msinwolc <msinwolc@gmail.com>
2024-10-18 18:32:16 +0800

    添加怪物信息

---
+++ src/constants/monters.js
@@ -0,0 +1,109 @@
+const monsters = {
+    "炼气期": [
+        { name: "野狼", health: 1200, attack: 70, defense: 30 },
+        { name: "灵蛇", health: 1300, attack: 65, defense: 35 },
+        { name: "幽火鬼婴", health: 1400, attack: 80, defense: 40 },
+        { name: "血蝠", health: 1250, attack: 75, defense: 28 },
+        { name: "赤焰蚁", health: 1100, attack: 60, defense: 32 },
+        { name: "鬼影猫", health: 1350, attack: 78, defense: 36 },
+        { name: "噬魂鼠", health: 1450, attack: 85, defense: 38 },
+        { name: "赤炎狐", health: 1380, attack: 77, defense: 35 },
+        { name: "冰晶虫", health: 1250, attack: 68, defense: 34 },
+        { name: "鬼火虫", health: 1300, attack: 70, defense: 32 }
+    ],
+    "筑基期": [
+        { name: "妖狐", health: 3500, attack: 130, defense: 70 },
+        { name: "玄冰蛇", health: 3700, attack: 140, defense: 75 },
+        { name: "地煞犬", health: 3400, attack: 125, defense: 68 },
+        { name: "银甲蚁", health: 3600, attack: 135, defense: 80 },
+        { name: "紫晶蜂", health: 3300, attack: 120, defense: 65 },
+        { name: "火鸦", health: 3800, attack: 145, defense: 78 },
+        { name: "金甲虫", health: 3400, attack: 130, defense: 70 },
+        { name: "冰灵鹿", health: 3550, attack: 128, defense: 73 },
+        { name: "石灵猿", health: 3600, attack: 135, defense: 75 },
+        { name: "幽冥豹", health: 3700, attack: 138, defense: 76 }
+    ],
+    "金丹期": [
+        { name: "毒蟒", health: 8000, attack: 220, defense: 110 },
+        { name: "火蜥蜴", health: 7800, attack: 210, defense: 105 },
+        { name: "冰魄蝠", health: 8200, attack: 225, defense: 115 },
+        { name: "玄金狼", health: 7900, attack: 215, defense: 108 },
+        { name: "赤焰豹", health: 8100, attack: 220, defense: 112 },
+        { name: "灵龟", health: 8500, attack: 200, defense: 130 },
+        { name: "血蝎", health: 7950, attack: 212, defense: 107 },
+        { name: "风刃雕", health: 8000, attack: 220, defense: 110 },
+        { name: "土灵熊", health: 8400, attack: 218, defense: 115 },
+        { name: "雷音猿", health: 8250, attack: 225, defense: 113 }
+    ],
+    "元婴期": [
+        { name: "火鸦", health: 15000, attack: 350, defense: 180 },
+        { name: "玄冰蛇王", health: 15500, attack: 365, defense: 185 },
+        { name: "地煞妖犬", health: 14800, attack: 340, defense: 175 },
+        { name: "金甲兽", health: 16000, attack: 380, defense: 190 },
+        { name: "雷翼鹰", health: 15200, attack: 355, defense: 182 },
+        { name: "玄土巨熊", health: 15800, attack: 370, defense: 188 },
+        { name: "冰魄蜈蚣", health: 15300, attack: 360, defense: 180 },
+        { name: "火焰豹", health: 15700, attack: 375, defense: 187 },
+        { name: "魔煞蛛", health: 16000, attack: 385, defense: 190 },
+        { name: "天灵豹", health: 15550, attack: 370, defense: 185 }
+    ],
+    "分神期": [
+        { name: "炎魔", health: 25000, attack: 500, defense: 250 },
+        { name: "冰霜巨人", health: 26000, attack: 520, defense: 260 },
+        { name: "裂地兽", health: 25500, attack: 510, defense: 255 },
+        { name: "雷鸣虎", health: 24800, attack: 495, defense: 245 },
+        { name: "紫电鹏", health: 26500, attack: 530, defense: 265 },
+        { name: "血魂蜥", health: 25200, attack: 505, defense: 250 },
+        { name: "风刃狼", health: 26000, attack: 520, defense: 260 },
+        { name: "毒灵蜂", health: 24500, attack: 490, defense: 240 },
+        { name: "烈焰牛", health: 25800, attack: 515, defense: 255 },
+        { name: "九幽犬", health: 26300, attack: 525, defense: 265 }
+    ],
+    "合体期": [
+        { name: "虚空蜃", health: 35000, attack: 750, defense: 380 },
+        { name: "玄冰魔狼", health: 36000, attack: 770, defense: 390 },
+        { name: "赤炎狮", health: 35500, attack: 760, defense: 385 },
+        { name: "暗影虎", health: 34500, attack: 740, defense: 370 },
+        { name: "金翼龙雕", health: 36500, attack: 780, defense: 395 },
+        { name: "毒焰蝎", health: 35000, attack: 750, defense: 380 },
+        { name: "风暴巨猿", health: 35800, attack: 765, defense: 385 },
+        { name: "雷魂灵猫", health: 34000, attack: 735, defense: 365 },
+        { name: "幻影蜈蚣", health: 35500, attack: 760, defense: 380 },
+        { name: "黑煞蛇", health: 36000, attack: 770, defense: 390 }
+    ],
+    "大乘期": [
+        { name: "天妖鹏", health: 70000, attack: 1400, defense: 700 },
+        { name: "焚天魔凰", health: 71000, attack: 1420, defense: 710 },
+        { name: "暗影魔狼", health: 70500, attack: 1410, defense: 705 },
+        { name: "冥河巨蟒", health: 69500, attack: 1390, defense: 695 },
+        { name: "苍穹巨龙", health: 72000, attack: 1440, defense: 720 },
+        { name: "玄冰玄龟", health: 70500, attack: 1410, defense: 705 },
+        { name: "血煞雷蝎", health: 71000, attack: 1420, defense: 710 },
+        { name: "烈焰吞天狮", health: 69500, attack: 1390, defense: 695 },
+        { name: "九幽魔虎", health: 71500, attack: 1430, defense: 715 },
+        { name: "虚空影鸦", health: 72000, attack: 1440, defense: 720 }
+    ],
+    "渡劫期": [
+        { name: "雷劫之龙", health: 100000, attack: 2000, defense: 1000 },
+        { name: "火劫凤凰", health: 102000, attack: 2040, defense: 1020 },
+        { name: "冰劫玄龟", health: 101000, attack: 2020, defense: 1010 },
+        { name: "雷劫魔神", health: 99000, attack: 1980, defense: 990 },
+        { name: "烈焰劫狮", health: 105000, attack: 2100, defense: 1050 },
+        { name: "黑暗劫影", health: 100500, attack: 2010, defense: 1005 },
+        { name: "冥劫灵蛇", health: 103000, attack: 2060, defense: 1030 },
+        { name: "九天雷鹰", health: 104000, attack: 2080, defense: 1040 },
+        { name: "绝地魔狼", health: 101500, attack: 2030, defense: 1015 },
+        { name: "天劫灵狐", health: 102500, attack: 2050, defense: 1025 }
+    ]
+};
+
+export const realmToMap = {
+    "炼气": "青云峰",
+    "筑基": "玄冰谷",
+    "金丹": "幻影迷林",
+    "元婴": "流沙禁地",
+    "分神": "赤炎火海",
+    "合体": "龙眠秘境",
+    "大乘": "无极仙境",
+    "渡劫": "天穹圣地",
+};
